bdfa14880beb68ff340f6798007d284231fab679
kassy11 <kotaro11kassy@gmail.com>
2021-04-13 18:37:41 +0900

    :memo: add java TemplateMethod readme.md

--- java/TemplateMethod/src/template/AbstractDisplay.java
+++ java/TemplateMethod/src/template/AbstractDisplay.java
@@ -1,12 +1,16 @@
 package template;
 
+// テンプレートクラス
+// インタフェースだと処理の大枠が書けないので抽象クラスを使う
+// インタフェースのdefaultメソッドを使っても、上書きされたらテンプレートの意味がなくなるのでダメ
 public abstract class AbstractDisplay {
   public abstract void open();
 
   public abstract void print();
 
   public abstract void close();
-  // 上記３つのメソッドを実装するのはサブクラスなので、display()が具体的kに何をするかは分からない
+  // 上記３つのメソッドを実装するのはサブクラスなので、display()が具体的に何をするかは分からない
+  // 処理の流れだけを記述しておく
   public final void display() {
     open();
     for (int i = 0; i < 5; i++) {
--- java/TemplateMethod/src/template/Main.java
+++ java/TemplateMethod/src/template/Main.java
@@ -2,7 +2,6 @@ package template;
 
 public class Main {
   public static void main(String[] args) {
-    // リスコフの原則に従う
     AbstractDisplay d1 = new CharDisplay('H');
     AbstractDisplay d2 = new StringDisplay("Hello, world");
     AbstractDisplay d3 = new StringDisplay("こんにちは");
